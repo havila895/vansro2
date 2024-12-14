fabbe57bf6f3efa0a58496c1f1fed0ac24b5960c
kk <kk@gmail.com>
2024-12-14 18:36:06 +0000

    1st commit

---
+++ First1.java
@@ -0,0 +1,11 @@
+public class First1
+{
+	public static void main(String args[])
+	{
+		System.out.println("Vansro Technologies");
+		System.out.println("Student Name");
+		System.out.println("Enter your Id:");
+		System.out.println("Enter password");
+	}
+}
+			
